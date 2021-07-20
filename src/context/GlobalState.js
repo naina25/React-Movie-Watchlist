bc71cc88f91efe1c1b0b70183850d308eca4fce8
naina <nanu02d8@gmail.com>
2021-07-20 20:41:09 +0530

    Use local storage for storing the watchlist data, hence the add to watchlist functionality completed

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,14 +2,24 @@ import React, { createContext, useEffect, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-  watchlist: [],
-  watched: [],
+  watchlist: localStorage.getItem("watchlist")
+    ? JSON.parse(localStorage.getItem("watchlist"))
+    : [],
+  watched: localStorage.getItem("watched")
+    ? JSON.parse(localStorage.getItem("watched"))
+    : [],
 };
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = (props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // adding to local storage
+  useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(state.watchlist));
+    localStorage.setItem("watched", JSON.stringify(state.watched));
+  }, [state]);
 
   // actions
   const addMovieToWatchlist = (movie) => {
